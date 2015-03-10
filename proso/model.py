940477a7e3e8a5b4e8270f51f2b080d9dd7eb938
Jan Papousek <jan.papousek@gmail.com>
2015-03-10 08:38:15 +0100

    minor simplification

--- proso/model.py
+++ proso/model.py
@@ -92,8 +92,8 @@ class ClusterEloModel(Model):
         item_nums = self._answers.get(('item', item), 0)
         self._users[user, cluster] = self._users.get((user, cluster), 0) + self.alpha(user_nums) * (correct - prediction)
         self._items[item] = self._items.get(item, 0) - self.alpha(item_nums) * (correct - prediction)
-        self._answers['user', user, cluster] = self._answers.get(('user', user, cluster), 0) + 1
-        self._answers['item', item] = self._answers.get(('item', item), 0) + 1
+        self._answers['user', user, cluster] = user_nums + 1
+        self._answers['item', item] = item_nums + 1
 
     def alpha(self, n):
         return self._alpha / (1 + self._dynamic_alpha * n)
